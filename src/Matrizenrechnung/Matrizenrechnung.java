e58668034efc794ec8ccf5e2a02bdbb0488a3bd1
lomberd2 <deinemamaistdumm@hotmail.com>
2020-12-09 10:13:49 +0100

    Added comments and expanded menu

--- src/Matrizenrechnung/Matrizenrechnung.java
+++ src/Matrizenrechnung/Matrizenrechnung.java
@@ -3,6 +3,13 @@ package Matrizenrechnung;
 public class Matrizenrechnung {
 
     //<editor-fold desc="Multiplication With One Number">
+
+    /**
+     * Multiplication of a Integer Matrix with a given number
+     * @param matrix The matrix that needs to be multiplied
+     * @param number The number by that the matrix gets multiplied
+     * @return returns multiplied matrix
+     */
     public static int[][] multiplicationWithOneNumber(int[][] matrix, int number){
         int[][] merkMatrix = new int[matrix.length][matrix[0].length];
         for(int i = 0; i < matrix.length; i++){
@@ -13,6 +20,12 @@ public class Matrizenrechnung {
         return merkMatrix;
     }
 
+    /**
+     * Multiplication of a Double Matrix with a given number
+     * @param matrix The matrix that needs to be multiplied
+     * @param number The number by that the matrix gets multiplied
+     * @return returns multiplied matrix
+     */
     public static double[][] multiplicationWithOneNumber(double[][] matrix, double number){
         double[][] merkMatrix = new double[matrix.length][matrix[0].length];
         for(int i = 0; i < matrix.length; i++){
@@ -37,6 +50,13 @@ public class Matrizenrechnung {
     //</editor-fold>
 
     //<editor-fold desc="Merge 2 Matrix">
+
+    /**
+     * Addition of two Integer Matrix with the same length
+     * @param matrix1 First Matrix to Add
+     * @param matrix2 Second Matrix to Add
+     * @return returns the added matrix
+     */
     public static int[][] mergeMatrix(int[][] matrix1, int[][] matrix2){
         if(!checkMatrixIsSame(matrix1, matrix2)){
             return null;
@@ -50,6 +70,12 @@ public class Matrizenrechnung {
         return mergedArr;
     }
 
+    /**
+     * Addition of two Double Matrix with the same length
+     * @param matrix1 First Matrix to Add
+     * @param matrix2 Second Matrix to Add
+     * @return returns the added matrix
+     */
     public static double[][] mergeMatrix(double[][] matrix1, double[][] matrix2){
         if(!checkMatrixIsSame(matrix1, matrix2)){
             return null;
@@ -65,6 +91,13 @@ public class Matrizenrechnung {
     //</editor-fold>
 
     //<editor-fold desc="Multiplication With Two Arrays">
+
+    /**
+     * Multiplication of two Integers Matrix with another
+     * @param matrix1 First Matrix to Multiply
+     * @param matrix2 Second Matrix to Multiply
+     * @return Multiplied Matrix
+     */
     public static int[][] multiplicateTwoMatrix(int[][] matrix1, int[][] matrix2){
         if(!checkMatrixIsSame(matrix1, matrix2)){
             return null;
@@ -72,6 +105,12 @@ public class Matrizenrechnung {
         return multiArr(matrix1, matrix2);
     }
 
+    /**
+     * Multiplication of two Double Matrix with another
+     * @param matrix1 First Matrix to Multiply
+     * @param matrix2 Second Matrix to Multiply
+     * @return Multiplied Matrix
+     */
     public static double[][] multiplicateTwoMatrix(double[][] matrix1, double[][] matrix2){
         if(!checkMatrixIsSame(matrix1, matrix2)){
             return null;
@@ -81,6 +120,13 @@ public class Matrizenrechnung {
     //</editor-fold>
 
     //<editor-fold desc="Methods">
+
+    /**
+     * Outsourced Multiplication of two Integer Matrices
+     * @param matrix1 First Matrix to Multiply
+     * @param matrix2 Second Matrix to Multiply
+     * @return Multiplied Matrix
+     */
     private static int[][] multiArr(int[][] matrix1, int[][] matrix2){
         int[][] multiArr = new int[matrix1.length][matrix1[0].length];
         for(int i = 0; i < matrix1.length; i++){
@@ -91,6 +137,12 @@ public class Matrizenrechnung {
         return multiArr;
     }
 
+    /**
+     * Outsourced Multiplication of two Double Matrices
+     * @param matrix1 First Matrix to Multiply
+     * @param matrix2 Second Matrix to Multiply
+     * @return Multiplied Matrix
+     */
     private static double[][] multiArr(double[][] matrix1, double[][] matrix2){
         double[][] multiArr = new double[matrix1.length][matrix1[0].length];
         for(int i = 0; i < matrix1.length; i++){
@@ -101,6 +153,12 @@ public class Matrizenrechnung {
         return multiArr;
     }
 
+    /**
+     * Outsourced function to check if the two Integer matrices are the same length.
+     * @param matrix1 First Matrix to check
+     * @param matrix2 Second Matrix to check
+     * @return true = all good, false = not the same length.
+     */
     private static boolean checkMatrixIsSame(int[][] matrix1, int[][] matrix2){
         if(matrix1.length != matrix2.length){
             return false;
@@ -111,6 +169,12 @@ public class Matrizenrechnung {
         return true;
     }
 
+    /**
+     * Outsourced function to check if the two Double matrices are the same length.
+     * @param matrix1 First Matrix to check
+     * @param matrix2 Second Matrix to check
+     * @return true = all good, false = not the same length.
+     */
     private static boolean checkMatrixIsSame(double[][] matrix1, double[][] matrix2){
         if(matrix1.length != matrix2.length){
             return false;
--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -63,20 +63,7 @@ public class Menu {
      */
     private static void mainMenu() throws Exception{
 
-        //Loading
-        console.writeWithTag("Main Menu loading");
-        Thread.sleep(500);
-
-        console.write(".");
-        Thread.sleep(500);
-
-        console.write(".");
-        Thread.sleep(500);
-
-        console.write(".");
-        Thread.sleep(500);
-        console.write("\n");
-
+        loadingEmulator("Main Menu");
 
         //MainLoop
         while(isRunning){
@@ -110,7 +97,7 @@ public class Menu {
     private static void printMainMenu(){
         console.writeLine("╔═════════════════════════════════════════════════════╗");
         console.writeLine("╠═──────────────────{  MAIN MENU  }──────────────────═╣");
-        console.writeLine("╠═─ 1 : STATISTIK (Varianz, Spannweite, Max, etc.) ──═╣");
+        console.writeLine("╠═─ 1 : Statistics (Min, Max, Span, etc.) ───────────═╣");
         console.writeLine("╠═─ 2 : Wachstum & Zerfall ──────────────────────────═╣");
         console.writeLine("╠═─ 3 : Quad. Gleichungen ───────────────────────────═╣");
         console.writeLine("╠═─ 4 : Vektorrechnung ──────────────────────────────═╣");
@@ -120,14 +107,93 @@ public class Menu {
     }
     //</editor-fold>
 
-
     private static void init(){
         console = new Console();
     }
 
     //<editor-fold desc="Statistik Stuff">
-    private static void statistik(){
+
+    /**
+     * Loading main menu for statistics stuff. Includes the loop and logik for the menu.
+     */
+    private static void statistik() throws Exception{
+        //Emulate Loading
+        loadingEmulator("Statistics menu");
+
+        while (true){
+            //Print Main Menu
+            printStatisticsMenu();
+
+            //Selection
+            console.write("\nSelect function: ");
+
+            switch (console.readInt()){
+                case 0 -> {
+                    console.writeLine("Returning to Main menu");
+                    Thread.sleep(1000);
+                    return;
+                }
+                case 1 -> {
+                    console.writeLine("Opening selection 1");
+                    Thread.sleep(500);
+                    minStatsSelected();
+                }
+                default -> {
+                    console.writeLine("Whoops something went wrong!", Colors.RED);
+                    Thread.sleep(1000);
+                }
+            }
+        }
+    }
+
+    /**
+     * Just prints the statistics menu
+     */
+    private static void printStatisticsMenu(){
+        console.writeLine("╔═════════════════════════════════════════════════════╗");
+        console.writeLine("╠═─────────────────{  Statistics  }──────────────────═╣");
+        console.writeLine("╠═─ 1 : Min (Get the lowest number from two) ────────═╣");
+        console.writeLine("╠═─ 2 : Max (Get the highest number from two) ───────═╣");
+        console.writeLine("╠═─ 3 : Span (Get the range between min and max) ────═╣");
+        console.writeLine("╠═─ 4 : Vektorrechnung ──────────────────────────────═╣");
+        console.writeLine("╠═─ 5 : Matrizenrechnung ────────────────────────────═╣");
+        console.writeLine("╠═─ 0 : Exit ────────────────────────────────────────═╣");
+        console.writeLine("╚═════════════════════════════════════════════════════╝");
+    }
+
+    /**
+     * Contains the logik for min function
+     */
+    private static void minStatsSelected(){
+
+    }
+
+    /**
+     * Contains the logik for max function
+     */
+    private static void maxStatsSelected(){
 
     }
     //</editor-fold>
+
+    /**
+     * Just a handy emulator for loading stuff
+     * @param loadingText Text to display
+     * @throws Exception Just an exception because of "Thread.sleep"
+     */
+    private static void loadingEmulator(String loadingText) throws Exception{
+        //Loading
+        console.writeWithTag(loadingText + " loading");
+        Thread.sleep(500);
+
+        console.write(".");
+        Thread.sleep(500);
+
+        console.write(".");
+        Thread.sleep(500);
+
+        console.write(".");
+        Thread.sleep(500);
+        console.write("\n");
+    }
 }
