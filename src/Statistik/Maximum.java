fe3a6845bbe5b6ce18e1691661ada9c76988f8f6
andrej85 <amenschtschikow@gmail.com>
2020-12-09 09:08:01 +0100

    Maximum und Minimum fertig

---
+++ src/Statistik/Maximum.java
@@ -0,0 +1,18 @@
+package Statistik;
+
+import oop.IO;
+
+public class Maximum {
+    public static int maximum(int zahl1, int zahl2) {
+        return Math.max(zahl1,zahl2);
+    }
+    public static double maximum(double zahl1, double zahl2){
+        return Math.max(zahl1, zahl2);
+    }
+    public static long maximum(long zahl1, long zahl2){
+        return Math.max(zahl1, zahl2);
+    }
+    public static float maximum(float zahl1, float zahl2){
+        return Math.max(zahl1, zahl2);
+    }
+}
---
+++ src/Statistik/Minimum.java
@@ -0,0 +1,16 @@
+package Statistik;
+
+public class Minimum {
+    public static int minimum(int zahl1, int zahl2){
+        return Math.min(zahl1,zahl2);
+    }
+    public static double minimum(double zahl1, double zahl2){
+        return Math.min(zahl1, zahl2);
+    }
+    public static long minimum(long zahl1, long zahl2){
+        return Math.min(zahl1, zahl2);
+    }
+    public static float minimum(float zahl1, float zahl2){
+        return Math.min(zahl1, zahl2);
+    }
+}
