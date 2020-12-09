1791eb523146772020ca6ed6f5ea3ee4dfcc69b5
andrej85 <amenschtschikow@gmail.com>
2020-12-09 10:02:02 +0100

    Spannweite fertig

---
+++ src/Statistik/Spannweite.java
@@ -0,0 +1,39 @@
+package Statistik;
+
+public class Spannweite {
+    /**
+     *
+     *  Find out the span from two Variables with int
+     */
+    public static int spannweite(int zahl1, int zahl2){
+        int spannweite = zahl1 - zahl2;
+        return spannweite;
+    }
+
+    /**
+     *
+     *  Find out the span from two Variables with double
+     */
+    public static double spannweite(double zahl1, double zahl2){
+        double spannweite = zahl1 - zahl2;
+        return spannweite;
+    }
+
+    /**
+     *
+     * Find out the span from two Variables with long
+     */
+    public static long spannweite(long zahl1, long zahl2){
+        long spannweite = zahl1 - zahl2;
+        return spannweite;
+    }
+
+    /**
+     *
+     * Find out the span from two variables with float
+     */
+    public static float spannweite(float zahl1, float zahl2){
+        float spannweite = zahl1 - zahl2;
+        return spannweite;
+    }
+}
