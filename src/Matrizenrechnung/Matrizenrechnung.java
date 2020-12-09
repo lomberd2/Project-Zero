c43f57dfbbab131b10c2b5136b48a0b5da8277f2
lomberd2 <deinemamaistdumm@hotmail.com>
2020-12-09 08:34:39 +0100

    Quick Fix for Andrej

---
+++ src/Matrizenrechnung/Matrizenrechnung.java
@@ -0,0 +1,13 @@
+package Matrizenrechnung;
+
+public class Matrizenrechnung {
+
+    public static int[][] multiplicationWithOneNumber(int[][] matrix, int number){
+        int[][] merkMatrix;
+
+
+        return merkMatrix;
+    }
+
+
+}
