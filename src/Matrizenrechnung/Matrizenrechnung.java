6d95567f5a23a30869d192bbe5dbe19e8e23c83a
lomberd2 <deinemamaistdumm@hotmail.com>
2020-12-09 09:13:19 +0100

    Matrizenrechnung Finished

--- src/Matrizenrechnung/Matrizenrechnung.java
+++ src/Matrizenrechnung/Matrizenrechnung.java
@@ -2,29 +2,24 @@ package Matrizenrechnung;
 
 public class Matrizenrechnung {
 
-
     //<editor-fold desc="Multiplication With One Number">
     public static int[][] multiplicationWithOneNumber(int[][] matrix, int number){
         int[][] merkMatrix = new int[matrix.length][matrix[0].length];
-
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[i].length; j++){
                 merkMatrix[i][j] = matrix[i][j] * number;
             }
         }
-
         return merkMatrix;
     }
 
     public static double[][] multiplicationWithOneNumber(double[][] matrix, double number){
         double[][] merkMatrix = new double[matrix.length][matrix[0].length];
-
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[i].length; j++){
                 merkMatrix[i][j] = matrix[i][j] * number;
             }
         }
-
         return merkMatrix;
     }
 
@@ -43,81 +38,88 @@ public class Matrizenrechnung {
 
     //<editor-fold desc="Merge 2 Matrix">
     public static int[][] mergeMatrix(int[][] matrix1, int[][] matrix2){
-        if(matrix1.length != matrix2.length){
+        if(!checkMatrixIsSame(matrix1, matrix2)){
             return null;
         }
-        if(matrix1[0].length != matrix2[0].length){
-            return null;
-        }
-
         int[][] mergedArr = new int[matrix1.length][matrix1[0].length];
-
         for(int i = 0; i < matrix1.length; i++){
             for(int j = 0; j < matrix1[i].length; j++){
                 mergedArr[i][j] = matrix1[i][j] + matrix2[i][j];
             }
         }
-
         return mergedArr;
     }
 
     public static double[][] mergeMatrix(double[][] matrix1, double[][] matrix2){
-        if(matrix1.length != matrix2.length){
+        if(!checkMatrixIsSame(matrix1, matrix2)){
             return null;
         }
-        if(matrix1[0].length != matrix2[0].length){
-            return null;
-        }
-
         double[][] mergedArr = new double[matrix1.length][matrix1[0].length];
-
         for(int i = 0; i < matrix1.length; i++){
             for(int j = 0; j < matrix1[i].length; j++){
                 mergedArr[i][j] = matrix1[i][j] + matrix2[i][j];
             }
         }
-
         return mergedArr;
     }
     //</editor-fold>
 
+    //<editor-fold desc="Multiplication With Two Arrays">
     public static int[][] multiplicateTwoMatrix(int[][] matrix1, int[][] matrix2){
-        if(matrix1.length != matrix2.length){
+        if(!checkMatrixIsSame(matrix1, matrix2)){
             return null;
         }
-        if(matrix1[0].length != matrix2[0].length){
-            return null;
-        }
-
-        int[][] multiArr = new int[matrix1.length][matrix1[0].length];
-
-        for(int i = 0; i < matrix1.length; i++){
-            for(int j = 0; j < matrix1[i].length; j++){
-                multiArr[i][j] = matrix1[i][j] * matrix2[i][j];
-            }
-        }
-
-        return multiArr;
+        return multiArr(matrix1, matrix2);
     }
 
     public static double[][] multiplicateTwoMatrix(double[][] matrix1, double[][] matrix2){
-        if(matrix1.length != matrix2.length){
+        if(!checkMatrixIsSame(matrix1, matrix2)){
             return null;
         }
-        if(matrix1[0].length != matrix2[0].length){
-            return null;
-        }
+        return  multiArr(matrix1, matrix2);
+    }
+    //</editor-fold>
 
-        double[][] multiArr = new double[matrix1.length][matrix1[0].length];
-
+    //<editor-fold desc="Methods">
+    private static int[][] multiArr(int[][] matrix1, int[][] matrix2){
+        int[][] multiArr = new int[matrix1.length][matrix1[0].length];
         for(int i = 0; i < matrix1.length; i++){
             for(int j = 0; j < matrix1[i].length; j++){
                 multiArr[i][j] = matrix1[i][j] * matrix2[i][j];
             }
         }
-
         return multiArr;
     }
 
+    private static double[][] multiArr(double[][] matrix1, double[][] matrix2){
+        double[][] multiArr = new double[matrix1.length][matrix1[0].length];
+        for(int i = 0; i < matrix1.length; i++){
+            for(int j = 0; j < matrix1[i].length; j++){
+                multiArr[i][j] = matrix1[i][j] * matrix2[i][j];
+            }
+        }
+        return multiArr;
+    }
+
+    private static boolean checkMatrixIsSame(int[][] matrix1, int[][] matrix2){
+        if(matrix1.length != matrix2.length){
+            return false;
+        }
+        if(matrix1[0].length != matrix2[0].length){
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean checkMatrixIsSame(double[][] matrix1, double[][] matrix2){
+        if(matrix1.length != matrix2.length){
+            return false;
+        }
+        if(matrix1[0].length != matrix2[0].length){
+            return false;
+        }
+        return true;
+    }
+    //</editor-fold>
 
 }
