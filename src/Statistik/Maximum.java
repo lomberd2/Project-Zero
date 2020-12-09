e4964fec5884705954aff1c8605421310949bef6
lomberd2 <deinemamaistdumm@hotmail.com>
2020-12-09 09:18:51 +0100

    Edited Max and Min

--- src/Statistik/Maximum.java
+++ src/Statistik/Maximum.java
@@ -1,18 +1,16 @@
 package Statistik;
 
-import oop.IO;
-
 public class Maximum {
-    public static int maximum(int zahl1, int zahl2) {
+    public static int getMaximum(int zahl1, int zahl2) {
         return Math.max(zahl1,zahl2);
     }
-    public static double maximum(double zahl1, double zahl2){
+    public static double getMaximum(double zahl1, double zahl2){
         return Math.max(zahl1, zahl2);
     }
-    public static long maximum(long zahl1, long zahl2){
+    public static long getMaximum(long zahl1, long zahl2){
         return Math.max(zahl1, zahl2);
     }
-    public static float maximum(float zahl1, float zahl2){
+    public static float getMaximum(float zahl1, float zahl2){
         return Math.max(zahl1, zahl2);
     }
 }
