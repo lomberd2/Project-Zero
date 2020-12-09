0d752397dcad94af718c6a5c4c51724db5a4ae10
andrej85 <amenschtschikow@gmail.com>
2020-12-09 11:00:27 +0100

    Edit on Maximum and Spannweite

--- src/Statistik/Minimum.java
+++ src/Statistik/Minimum.java
@@ -1,15 +1,42 @@
 package Statistik;
 
 public class Minimum {
+    /**
+     * Find out the lowest number with int
+     * @param zahl1 Variable one
+     * @param zahl2 Variable two
+     * @return Lowest number
+     */
     public static int getMinimum(int zahl1, int zahl2){
         return Math.min(zahl1,zahl2);
     }
+
+    /**
+     * Find out the lowest number with double
+     * @param zahl1 Variable one
+     * @param zahl2 Variable two
+     * @return Lowest number
+     */
     public static double getMinimum(double zahl1, double zahl2){
         return Math.min(zahl1, zahl2);
     }
+
+    /**
+     * Find out the lowest number with long
+     * @param zahl1 Variable one
+     * @param zahl2 Variable two
+     * @return Lowest number
+     */
     public static long getMinimum(long zahl1, long zahl2){
         return Math.min(zahl1, zahl2);
     }
+
+    /**
+     * Find out the lowest number with float
+     * @param zahl1 Variable one
+     * @param zahl2 Variable two
+     * @return Lowest number
+     */
     public static float getMinimum(float zahl1, float zahl2){
         return Math.min(zahl1, zahl2);
     }
