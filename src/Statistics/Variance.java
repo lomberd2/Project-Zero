570609dd548e07159963383440084d243832ed4e
janderlp <71753598+janderlp@users.noreply.github.com>
2020-12-16 12:40:42 +0100

    added Variance

--- src/Statistics/Variance.java
+++ src/Statistics/Variance.java
@@ -9,16 +9,18 @@ public class Variance {
      */
     public static double getVariance(double[] variance){
         double sumVar = 0;
-        double sumAtm = 0;
+        double sumArm = 0;
         double product;
         double varianceValue;
 
         for (int i = 0; i < variance.length; i++){
-            sumAtm += variance[i];
+            sumArm += variance[i];
         }
 
+        sumArm = sumArm / variance.length;
+
         for (int i = 0; i < variance.length; i++){
-            sumVar = (variance[i] - sumAtm);
+            sumVar = (variance[i] - sumArm);
         }
         product = sumVar * sumVar;
         varianceValue = product / variance.length;
