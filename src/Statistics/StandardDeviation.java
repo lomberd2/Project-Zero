3bd211797f0614eccb10cf5aefbb60ae6161f266
janderlp <71753598+janderlp@users.noreply.github.com>
2020-12-16 12:52:47 +0100

    added StandardDeviation

---
+++ src/Statistics/StandardDeviation.java
@@ -0,0 +1,17 @@
+package Statistics;
+
+public class StandardDeviation {
+
+    /**
+     * Takes the Array variance and uses the methode getVariance
+     * to calculate the StandardDeviation
+     * @param variance Array
+     * @return StandardDeviation
+     */
+    public static double getStandardDeviation(double [] variance){
+
+        double standardDeviation = Math.sqrt(Variance.getVariance(variance));
+
+        return standardDeviation;
+    }
+}
