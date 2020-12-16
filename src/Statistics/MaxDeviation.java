b963ee7a586fadc61046d8db8f5f927a392402ae
janderlp <71753598+janderlp@users.noreply.github.com>
2020-12-16 14:49:17 +0100

    MaxDeviation added and changed

--- src/Statistics/MaxDeviation.java
+++ src/Statistics/MaxDeviation.java
@@ -1,13 +1,13 @@
 package Statistics;
 
 public class MaxDeviation {
-    /**
+    /*
      * Get the average of the numbers to calculate the Variance
      * @param numbers Variable one
      * @param average Variable two
      * @param number The number it will get divided t get the average
      * @return Gives you the Average
-     */
+
     public static int[] getAverage(int[] numbers, int[] average, int number){
         for (int i = 0; i < numbers.length; i++){
             average = numbers ;
@@ -28,5 +28,24 @@ public class MaxDeviation {
 
 
         return getMaxDeviation(zahl1, zahl2);
+    }
+     */
+
+    public static double getMaxDeviation(double [] numberChain){
+
+        double min = numberChain[0];
+        double max = numberChain[0];
+
+        for(int i = 0; i < numberChain.length; i++) {
+            if(numberChain[i] < min){
+                min = numberChain[i];
+            } else if(numberChain[i] > max){
+                max = numberChain[i];
+            }
+
+        }
+
+        double maxDeviation = max - min;
+        return maxDeviation;
     }
 }
