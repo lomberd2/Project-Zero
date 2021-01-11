596a6b304ec36721e04d77d408ce445ef2e5fffa
janderlp <71753598+janderlp@users.noreply.github.com>
2021-01-11 09:45:49 +0100

    median fixed

--- src/Statistics/Median.java
+++ src/Statistics/Median.java
@@ -9,6 +9,16 @@ public class Median {
      */
     public static double getMedian(double [] numberChain) {
 
+        for (int j = 0; j < numberChain.length - 1; j++) {
+            for (int i = 0; i < numberChain.length - 1; i++) {
+                if (numberChain[i] > numberChain[i + 1]) {
+                    double c = numberChain[i];
+                    numberChain[i] = numberChain[i + 1];
+                    numberChain[i + 1] = c;
+                }
+            }
+        }
+
         if (numberChain.length % 2 == 0) {
             return (numberChain[(numberChain.length/2 - 1)] + numberChain[(numberChain.length/2)]) / 2;
         } else {
